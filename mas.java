31fa6a8668943d6359552147841c84399737fccc
Masilajr <mutuajoshua3436@gmail.com>
2024-01-19 14:09:36 +0300

    JAVA1

---
+++ mas.java
@@ -0,0 +1,13 @@
+public class Josh {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++)
+        {
+            System.out.println(i);
+
+            for (int j = 0; j < 6; j = j+2){
+                System.out.println(j);
+            }
+
+        }
+    }
+}
